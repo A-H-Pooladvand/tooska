9684ba3e4ea4d838296ccb1ad69a15768ecd4605
Amirhossein Pooladvand <a.h.pooladvand@gmail.com>
2018-02-22 18:23:20 +0330

    remove trash

--- resources/views/_seo/seo-analysis.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include 'views/public/header_view.php';
-  include 'views/public/menu_view.php';
-  include 'views/seo-analysis/analysis_heading_image_view.php';
-  include 'views/seo-analysis/detailed_analysis_view.php';
-
-  include 'views/public/footer_view.php';
-  include 'views/public/ending.php';
-?>
